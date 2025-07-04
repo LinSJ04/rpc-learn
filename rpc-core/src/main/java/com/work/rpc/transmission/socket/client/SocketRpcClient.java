c917e57e96da7e03a4b7c0979d57d7fa49b82215
林思佳 <2318988232@qq.com>
2025-07-04 15:07:15 +0800

    基于socket实现客户端发送请求

---
+++ rpc-core/src/main/java/com/work/rpc/transmission/socket/client/SocketRpcClient.java
@@ -0,0 +1,28 @@
+package com.work.rpc.transmission.socket.client;
+
+import com.work.rpc.dto.RpcReq;
+import com.work.rpc.dto.RpcResp;
+import com.work.rpc.transmission.RpcClient;
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+
+@Slf4j
+public class SocketRpcClient implements RpcClient {
+    public RpcResp<?> sendReq(RpcReq req) {
+        try (Socket socket = new Socket("localhost", 8888)) {
+            ObjectOutputStream objectOutputStream = new ObjectOutputStream(socket.getOutputStream());
+            objectOutputStream.writeObject(req);
+            // flush()强制将缓冲区中的任何尚未写出的数据立即写入到目标设备或文件中
+            objectOutputStream.flush();
+
+            ObjectInputStream objectInputStream = new ObjectInputStream(socket.getInputStream());
+            return (RpcResp<?>) objectInputStream.readObject();
+        } catch (Exception e) {
+            log.error("发送rpc请求失败", e);
+        }
+        return null;
+    }
+}
