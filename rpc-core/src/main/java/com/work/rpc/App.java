394d8a0f381dfbe6390bf2bfb994f8610aee1b60
林思佳 <2318988232@qq.com>
2025-07-04 13:49:38 +0800

    first commit

---
+++ rpc-core/src/main/java/com/work/rpc/App.java
@@ -0,0 +1,9 @@
+package com.work.rpc;
+
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ test-api/src/main/java/com/work/rpc/api/UserService.java
@@ -0,0 +1,5 @@
+package com.work.rpc.api;
+
+public interface UserService {
+    User getUser(Long id);
+}
