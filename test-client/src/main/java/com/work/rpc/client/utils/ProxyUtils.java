53b68940c21e6cd5d76837c1f8c02984ab75b57e
LinSJ04 <2318988232@qq.com>
2025-07-13 10:25:36 +0800

    引入zookeeper实现服务注册的原因分析

--- test-client/src/main/java/com/work/rpc/client/utils/ProxyUtils.java
+++ test-client/src/main/java/com/work/rpc/client/utils/ProxyUtils.java
@@ -5,6 +5,12 @@ import com.work.rpc.transmission.RpcClient;
 import com.work.rpc.transmission.socket.client.SocketRpcClient;
 
 public class ProxyUtils {
+    // 指定了ip和端口号
+    // 但是实际应用中，可能会有多个服务器提供服务
+    // 并且地址可能会有变化
+    // 服务器的个数也会变化
+    // 需要有一套发现机制，来发现服务器的地址
+    // 提供服务的服务器需要注册服务，此处的服务和现在的Provider不同，指的是整个应用服务
     private static final RpcClient rpcClient = new SocketRpcClient("localhost", 8888);
     private static final RpcClientProxy rpcClientProxy = new RpcClientProxy(rpcClient);
 
