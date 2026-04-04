466224fae5a5288a182d6e040408ff329d4d38d1
LinSJ04 <2318988232@qq.com>
2026-04-04 09:59:34 +0800

    [test] modify local vm host

--- rpc-core/src/main/java/com/work/rpc/constant/RpcConstant.java
+++ rpc-core/src/main/java/com/work/rpc/constant/RpcConstant.java
@@ -3,7 +3,7 @@ package com.work.rpc.constant;
 public class RpcConstant {
     public static final int SERVER_PORT = 9999; // 服务端口号
 
-    public static final String ZK_IP = "192.168.91.100"; // Zookeeper地址
+    public static final String ZK_IP = "192.168.230.131"; // Zookeeper地址
 
     public static final int ZK_PORT = 2181; // Zookeeper端口号
 
